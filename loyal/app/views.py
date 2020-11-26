13cee170d344cc0f6a3b50a84bfa75f031b6d730
Danil Andreev <d2234@mail.ru>
2020-11-26 17:15:20 +0300

    very bad hardcode

--- loyal/app/views.py
+++ loyal/app/views.py
@@ -1,25 +1,42 @@
+import uuid
+
 from aiohttp import web, hdrs
 from .responses import ok
 
 __all__ = ("register_views",)
+
+ID = 10
 
 
 async def ping_view(_: web.Request) -> web.Response:
     return ok(message="pong")
 
 
-async def register_v1_view(_: web.Request) -> web.Response:
-    return ok(message="register")
+async def register_user_view(_: web.Request) -> web.Response:
+    global ID
+    data = {
+        "user_id": ID,
+        "access_token": str(uuid.uuid4())
+    }
+    ID = ID + 1
+    return ok(data=data)
 
 
-async def login_v1_view(_: web.Request) -> web.Response:
-    return ok(message="login")
+async def login_user_view(_: web.Request) -> web.Response:
+    global ID
+    data = {
+        "user_id": ID,
+        "access_token": str(uuid.uuid4())
+    }
+    ID = ID + 1
+    return ok(data=data)
 
 
 def register_api_v1(root: web.Application) -> None:
     v1_app = web.Application()
-    v1_app.router.add_post("/register", register_v1_view)
-    v1_app.router.add_post("/login", login_v1_view)
+
+    v1_app.router.add_post("/users", register_user_view)
+    v1_app.router.add_post("/tokens", login_user_view)
 
     root.add_subapp("/api/v1/", v1_app)
 

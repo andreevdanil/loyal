29d176f08f17bb6f1241477b409fdbce2ff8aa63
Danil Andreev <d2234@mail.ru>
2020-12-07 00:28:53 +0300

    fixed eth client

--- loyal/infrastructure/ethereum.py
+++ loyal/infrastructure/ethereum.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Dict, TypedDict
 
 import attr
+import solcx
 from solcx import compile_source
 from web3 import Web3, WebsocketProvider
 from web3.contract import ContractFunctions
@@ -39,8 +40,8 @@ def make_eth_client(config: EthConfig) -> EthereumService:
     provider = WebsocketProvider(config["node_url"])
     w3 = Web3(provider)
 
-    contract_path = CURRENT_DIR / "contract.sol"
-    abi = compile_abi(contract_path)
+    # contract_path = CURRENT_DIR / "contract.sol"
+    abi = [{'inputs': [], 'stateMutability': 'nonpayable', 'type': 'constructor'}, {'anonymous': False, 'inputs': [{'indexed': True, 'internalType': 'address', 'name': 'from', 'type': 'address'}, {'indexed': True, 'internalType': 'address', 'name': 'to', 'type': 'address'}, {'indexed': False, 'internalType': 'int256', 'name': 'value', 'type': 'int256'}], 'name': 'Transfer', 'type': 'event'}, {'inputs': [{'internalType': 'address', 'name': '', 'type': 'address'}], 'name': 'balances', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': 'addr', 'type': 'address'}], 'name': 'getBalance', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'offset', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': 'to', 'type': 'address'}, {'internalType': 'int256', 'name': 'amount', 'type': 'int256'}], 'name': 'transfer', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}]
     contract = w3.eth.contract(config["contract_address"], abi=abi)
 
     return EthereumService(contract.functions)

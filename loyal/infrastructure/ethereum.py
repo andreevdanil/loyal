4372732a2f2dc0b70dc7deb8e2ea4c0e45fba368
Danil Andreev <d2234@mail.ru>
2020-12-15 22:23:18 +0300

    added new contract

--- loyal/infrastructure/ethereum.py
+++ loyal/infrastructure/ethereum.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from typing import Dict, TypedDict
 
 import attr
-import solcx
-from solcx import compile_source
 from web3 import Web3, WebsocketProvider
 from web3.contract import ContractFunctions
 
@@ -17,12 +15,6 @@ __all__ = (
 CURRENT_DIR = Path(__file__).parent
 
 
-def compile_abi(contract_path: Path) -> Dict:
-    contract = contract_path.read_text()
-    compiled_file = compile_source(contract, output_values=["abi"]).popitem()
-    return compiled_file[1]["abi"]
-
-
 class EthConfig(TypedDict):
     node_url: str
     contract_address: str
@@ -33,7 +25,8 @@ class EthereumService:
     functions: ContractFunctions
 
     def get_balance(self, account_address: str) -> float:
-        balance = self.functions.getBalance(account_address).call()
+        adr = Web3.toChecksumAddress(account_address)
+        balance = self.functions.getBalance(adr).call()
         return balance
 
 
@@ -43,7 +36,6 @@ def make_eth_client(config: EthConfig) -> EthereumService:
 
     contract_path = CURRENT_DIR / "loyal.json"
     contract = json.loads(contract_path.read_bytes())
-    # abi = [{'inputs': [], 'stateMutability': 'nonpayable', 'type': 'constructor'}, {'anonymous': False, 'inputs': [{'indexed': True, 'internalType': 'address', 'name': 'from', 'type': 'address'}, {'indexed': True, 'internalType': 'address', 'name': 'to', 'type': 'address'}, {'indexed': False, 'internalType': 'int256', 'name': 'value', 'type': 'int256'}], 'name': 'Transfer', 'type': 'event'}, {'inputs': [{'internalType': 'address', 'name': '', 'type': 'address'}], 'name': 'balances', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': 'addr', 'type': 'address'}], 'name': 'getBalance', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'name': 'offset', 'outputs': [{'internalType': 'int256', 'name': '', 'type': 'int256'}], 'stateMutability': 'view', 'type': 'function'}, {'inputs': [{'internalType': 'address', 'name': 'to', 'type': 'address'}, {'internalType': 'int256', 'name': 'amount', 'type': 'int256'}], 'name': 'transfer', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}]
     contract = w3.eth.contract(config["contract_address"], abi=contract["abi"])
 
     return EthereumService(contract.functions)
